edeb99d7587be1365c386c6a441515531245daae
duydev245 <danniduy.work@gmail.com>
2025-08-13 06:23:47 +0700

    first commit

---
+++ src/shared/constants/role.constant.ts
@@ -0,0 +1,16 @@
+export const RoleName = {
+  SuperAdmin: 'SUPERADMIN',
+  Admin: 'ADMIN',
+  ProjectManager: 'PROJECTMANAGER',
+  Employee: 'EMPLOYEE',
+} as const
+
+export const HTTPMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  PATCH: 'PATCH',
+  OPTIONS: 'OPTIONS',
+  HEAD: 'HEAD',
+} as const
---
+++ src/shared/helpers.ts
@@ -0,0 +1,10 @@
+import { Prisma } from "generated/prisma"
+
+// Type Predicate
+export function isUniqueConstraintPrismaError(error: any): error is Prisma.PrismaClientKnownRequestError {
+  return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002'
+}
+
+export function isNotFoundPrismaError(error: any): error is Prisma.PrismaClientKnownRequestError {
+  return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025'
+}
