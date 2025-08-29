8aa66cf1cff52cc85644594ebd8e43ee4583dadd
duydev245 <danniduy.work@gmail.com>
2025-08-29 11:49:00 +0700

    CORRECT: CHANGE ERROR CODE

--- src/shared/error.ts
+++ src/shared/error.ts
@@ -1,12 +1,7 @@
-import { ConflictException, NotFoundException, UnprocessableEntityException } from "@nestjs/common"
+import { ConflictException, NotFoundException, UnauthorizedException } from "@nestjs/common"
 
 export const NotFoundRecordException = new NotFoundException('Error.NotFound')
 
-export const InvalidPasswordException = new UnprocessableEntityException([
-  {
-    message: 'Error.InvalidPassword',
-    path: 'password',
-  },
-])
+export const InvalidPasswordException = new UnauthorizedException({ code: 'AUTH_INVALID_PASSWORD', message: 'Invalid password' })
 
 export const VersionConflictException = new ConflictException('Error.VersionConflict')
