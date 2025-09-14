daff11a3c41e3cb41a4564ab0f99a502f589c412
duydev245 <danniduy.work@gmail.com>
2025-09-14 22:47:11 +0700

    CODING: send email for new account

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -5,8 +5,8 @@ import fs from "fs";
 import path from "path";
 
 // Load the HTML template once when the module is initialized
-
 const template = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), 'utf-8');
+const newAccountTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/new_account_email.html'), 'utf-8');
 
 @Injectable()
 export class EmailService {
@@ -20,6 +20,23 @@ export class EmailService {
         const subject = '[FLOW-PILOT] OTP CODE FOR VERIFICATION';
         // Replace placeholders in the template with actual values
         const htmlContent = template.replaceAll('{{subject}}', subject).replace('{{code}}', payload.code);
+
+        return await this.resend.emails.send({
+            from: 'Flow Pilot <no-reply@flowpilot.io.vn>',
+            to: [payload.email],
+            subject: subject,
+            html: htmlContent,
+        });
+    }
+
+    async sendNewAccountEmail(payload: { email: string, name: string, password: string }) {
+        const subject = '[FLOW-PILOT] YOUR NEW ACCOUNT DETAILS';
+        // Replace placeholders in the template with actual values
+        const htmlContent = newAccountTemplate
+            .replaceAll('{{subject}}', subject)
+            .replace('{{name}}', payload.name)
+            .replace('{{email}}', payload.email)
+            .replace('{{password}}', payload.password);
 
         return await this.resend.emails.send({
             from: 'Flow Pilot <no-reply@flowpilot.io.vn>',
