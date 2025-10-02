9bdc74247b5de257eedabbdd77243645eeac5e3b
duydev245 <danniduy.work@gmail.com>
2025-10-02 17:04:40 +0700

    CODING: Department CRUD

--- src/routes/department/department.module.ts
+++ src/routes/department/department.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { DepartmentService } from './department.service';
-import { DepartmentController } from './department.controller';
+import { Module } from '@nestjs/common'
+import { DepartmentService } from './department.service'
+import { DepartmentController } from './department.controller'
+import { DepartmentRepository } from './department.repo'
+import { SharedModule } from 'src/shared/shared.module'
 
 @Module({
+  imports: [SharedModule],
   controllers: [DepartmentController],
-  providers: [DepartmentService],
+  providers: [DepartmentService, DepartmentRepository],
+  exports: [DepartmentService, DepartmentRepository],
 })
-export class DepartmentModule {}
+export class DepartmentModule { }
